e35686d56ce52aea1d27157c2b017525fe9a88b4
Darwel Quintero <darwel34@gmail.com>
2017-06-22 20:06:44 -0430

    ejemplo de uso de promesas

---
+++ 6_procesos_acincronos/promesas.js
@@ -0,0 +1,40 @@
+function get(URL){
+  return new Promise( (resolve, reject) =>{
+    const xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function(){
+      const DONE = 4;
+      const OK = 200;
+
+      if(DONE === this.readyState){
+        if(OK === this.status){
+          resolve(JSON.parse(this.responseText));
+        } else{
+          reject(new Error(`Se produjo un error al realizar el request ${this.status}`))
+        }
+      }
+    }
+
+    xhr.open('GET', URL);
+    xhr.send(null);    
+  });
+}
+
+get('https://swapi.co/api/people/1/')
+.then(function(response){
+  let luke = {};
+  luke = response;
+  let strUrl = JSON.stringify(luke.homeworld);
+  let url = `https${strUrl.substring(5,strUrl.length -1)}`;
+  get(url)
+  .then(function(response){
+    luke.homeworld = response;
+    console.log(`${luke.name} nacio en ${luke.homeworld.name}`);
+  })
+  .catch(function(err){
+    console.log(err);
+  });
+})
+.catch(function(err){
+  console.log(err);
+});
